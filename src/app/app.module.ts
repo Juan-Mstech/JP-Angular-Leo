98053ab83b0302273e119c481ddc128003c02dc8
Juan <jponce@mstech.la>
2019-07-26 17:28:27 -0300

    26.07.19-17.28

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ViewmodeloComponent } from './viewmodelo/viewmodelo.component';
-
+import { EjpropertybindingComponent } from './ejpropertybinding/ejpropertybinding.component';
+import { EjmetodoComponent } from './ejmetodo/ejmetodo.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ViewmodeloComponent
+    EjpropertybindingComponent,
+    EjmetodoComponent
   ],
   imports: [
     BrowserModule,
@@ -18,3 +19,5 @@ import { ViewmodeloComponent } from './viewmodelo/viewmodelo.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
